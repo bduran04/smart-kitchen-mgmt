7d47031df8b1ee3d0109190b2559b6c9806b599d
Sean Pacheco <101679954+Seanpacheco@users.noreply.github.com>
2025-02-27 19:42:22 -0500

    CRM-37-add-create-table-sql-for-waste-table (#26)
    * CRM-37-add-create-table-sql-for-waste-table
    - Add create.sql for waste table
    
    * CRM-37-add-create-table-sql-for-waste-table
    - added quantity column to waste table

---
+++ management_service/app/DB/sql/waste/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE waste (
+    wasteID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    stockID INT NOT NULL,
+    reason TEXT,
+    wasteTimestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    quantity INT NOT NULL,
+    CONSTRAINT fk_waste_stockID 
+      FOREIGN KEY (stockID) 
+      REFERENCES stock(stockID)
+);
