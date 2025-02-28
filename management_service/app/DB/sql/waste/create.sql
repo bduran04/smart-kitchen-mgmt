0f0c7f49d6dc96502f4d636c95325a35c3b0d98d
Sean Pacheco <101679954+Seanpacheco@users.noreply.github.com>
2025-02-27 20:35:42 -0500

    CRM-50-database-add-enum-to-the-waste-table (#30)
    -added wasteReason enum
    -changed reason data type to enum

--- management_service/app/DB/sql/waste/create.sql
+++ management_service/app/DB/sql/waste/create.sql
@@ -1,7 +1,12 @@
+CREATE TYPE wasteReason AS ENUM (
+'Expired', 
+'Over-prepped'
+);
+
 CREATE TABLE waste (
     wasteID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     stockID INT NOT NULL,
-    reason TEXT,
+    reason wasteReason NOT NULL,
     wasteTimestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     quantity INT NOT NULL,
     CONSTRAINT fk_waste_stockID 
