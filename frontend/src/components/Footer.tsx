cb120381ccd04a008bdd4c6ca3b82bca984ee8b9
Gilbert Matos <gilbertplayx@hotmail.com>
2025-03-05 18:16:40 -0500

    CRM 67 frontend fix the minified hydration bug (#42)
    * Created a components folder in the source folder. Added 3 files for components I'll be making to use in the sidenavbar component. Updated the css file with some styles we'll be using throughout the dev cycle of the app.
    
    * this file keeps showing up as recently deleted even even after adding it and marking it as such.
    
    * CRM-67 Fixed a minified hydration issue where the data object was being used in a state variable to pass down to an element. This kept causing the time to mismatch the actual time after what I think is a component mounting, and unmounting. Could be wrong, but that's all i can think about.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,22 +2,24 @@
 import { useState, useEffect } from "react";
 
 export default function Footer() {
-  const [date, setDate] = useState(new Date());
-
-  const formatter = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "full",
-    timeStyle: "short",
-  });
-
+   
+  const [formattedDate, setFormattedDate] = useState("");
+  const updateDate =()=>{
+    const date = new Date();
+    const formatter = new Intl.DateTimeFormat("en-US", {
+      dateStyle: "full",
+      timeStyle: "short",
+    });    
+    const newStringDate = formatter.format(date).replace(" at", "")
+    setFormattedDate(newStringDate)
+  } 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setDate(() => new Date());
-    }, 1000);
+    const timer = setInterval(updateDate, 1000);
     return () => clearInterval(timer);
   }, []);
   return (
     <footer className="footer-component">
-      <span>{<pre>{formatter.format(date).replace(" at", "")}</pre>}</span>
+      <span>{formattedDate}</span>
     </footer>
   );
 }
