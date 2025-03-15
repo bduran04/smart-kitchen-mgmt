cda64bd876fa49eb8d46b5965918e64bae2c607a
Gilbert Matos <gilbertplayx@hotmail.com>
2025-03-15 13:58:05 -0400

    CRM-108 fe remove extra slash from the url string (#76)
    * Created a components folder in the source folder. Added 3 files for components I'll be making to use in the sidenavbar component. Updated the css file with some styles we'll be using throughout the dev cycle of the app.
    
    * this file keeps showing up as recently deleted even even after adding it and marking it as such.
    
    * CRM-20 All changes pulled into main. I've also gone ahead and extracted the styles of orderreceiptmanager into its own css module.
    
    * CRM-81 Created a CustomMenuSelection component for creating menus and its own css module. I've gone ahead and further streamlined how menus are setup, and styled. I've also included a very useful update to the frontend READ me file that goes over the process of using this file, and other things for those using this component and css modules. This will help make menus a lot simpler and faster.
    
    * CRM-81 Made the orders container scroll horizontally. Orders display most of their data correctly, except for the time, which is currently missing, and the cost total.
    
    * CRM-81 Removed all unnecessary comments and fixed implementing the fetching logic with the menu.
    
    * CRM-108 Removed the extra slash ---> / <--- from the URL string causing the an error during fetching. The issue is now fixed.
    
    * CRM-108 added the missing api/ portion the URL string was missing.

--- frontend/src/app/orderTracking/page.tsx
+++ frontend/src/app/orderTracking/page.tsx
@@ -8,10 +8,10 @@ import { SelectionObject } from "@/components/SelectionObject";
 
 export default function OrderTrackingPage() {
   const {currentSelection, isCurrentSelection, setCurrentSelection} =useSelection()
-  let fetchString = "/orders?"
+  let fetchString = "api/orders?"
   if(!isCurrentSelection("none")){
     const showCompletedOrders = isCurrentSelection("Completed Orders")
-    fetchString = `/orders?completed=${showCompletedOrders}&orderItemsDetails=true`
+    fetchString = `api/orders?completed=${showCompletedOrders}&orderItemsDetails=true`
   }
   const { data } = useFetch<{ orders: Order[] }>(fetchString);
   const selObject: SelectionObject={ setCurrentSelection: setCurrentSelection,
